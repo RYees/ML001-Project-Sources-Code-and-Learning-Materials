a3a040a5f7cd53d1e73261c1b69489e6979509a9
rehmet yeshanew <ryfgmz87@gmail.com>
2024-03-24 10:10:10 -0400

    boston prediction

--- Notebooks Used in the course/Stock Price Predictor/app.py
+++ Notebooks Used in the course/Stock Price Predictor/app.py
@@ -1,10 +1,23 @@
-from flask import Flask, render_template  
+from logging import debug
+from flask import Flask, render_template, request  
+import utils
 
 app = Flask(__name__)  
 
 @app.route('/') 
 def home(): 
     return render_template('index.html') 
+
+@app.route('/predict/', methods = ["GET", "POST"])
+def predict():
+    if request.method == "POST":
+        Open = request.form.get("Open")
+        High = request.form.get("High")
+        Low = request.form.get("Low")
+        Volume = request.form.get("Volume")
+    prediction = utils.preprocess(Open, High, Low, Volume)
+
+    return render_template('prediction.html', prediction=prediction)
 
 
 if __name__ == "__main__": 
