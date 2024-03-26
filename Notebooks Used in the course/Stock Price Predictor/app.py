05d56bd50755572cdb8c37fb5e4173bd0b8178e0
rehmet yeshanew <ryfgmz87@gmail.com>
2024-03-25 21:40:31 -0400

    flask template

--- Notebooks Used in the course/Stock Price Predictor/app.py
+++ Notebooks Used in the course/Stock Price Predictor/app.py
@@ -10,13 +10,16 @@ def home():
 
 @app.route('/predict/', methods = ["GET", "POST"])
 def predict():
+    prediction = 0
     if request.method == "POST":
         Open = request.form.get("Open")
         High = request.form.get("High")
         Low = request.form.get("Low")
         Volume = request.form.get("Volume")
-    prediction = utils.preprocess(Open, High, Low, Volume)
 
+        prediction = utils.preprocess(Open, High, Low, Volume)
+        # return render_template('prediction.html', prediction=prediction)
+    #Handle the case when the request method is not POST
     return render_template('prediction.html', prediction=prediction)
 
 
--- Notebooks Used in the course/Stock Price Predictor/utils.py
+++ Notebooks Used in the course/Stock Price Predictor/utils.py
@@ -1,8 +1,18 @@
 import joblib
 import numpy as np
+import os
+
+file_path = os.path.join(os.path.dirname("/home/ek/Documents/10Xtutor/ML_Journey/ML001-Project-Sources-Code-and-Learning-Materials/Notebooks Used in the course/Stock Price Predictor/"), "rigidmodel.pkl")
 
 def preprocess(Open, High, Low, Volume):
+    # Convert input values to numeric types
+    Open = float(Open)
+    High = float(High)
+    Low = float(Low)
+    Volume = float(Volume)
+
     test_data = np.array([[Open, High, Low, Volume]])
-    trained_model = joblib.load("rigidmodel.pkl")
+    trained_model = joblib.load(file_path)
     prediction = trained_model.predict(test_data)
+    print(prediction)
     return prediction
